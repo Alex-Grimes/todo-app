0290918e6e4d9335e829cc320766310f9e44a9ae
Alexander Grimes <66704965+Alex-Grimes@users.noreply.github.com>
2022-01-12 16:50:04 -0500

    progress
    styled application and added functions for tracking percentages of list completion

---
+++ app/controllers/todo_items_controller.rb
@@ -0,0 +1,68 @@
+class TodoItemsController < ApplicationController
+  before_action :set_todo_list
+  before_action :set_todo_item, only: [:show, :edit, :update, :destroy]
+
+  # GET todo_lists/1/todo_items
+  def index
+    @todo_items = @todo_list.todo_items
+  end
+
+  # GET todo_lists/1/todo_items/1
+  def show
+  end
+
+  # GET todo_lists/1/todo_items/new
+  def new
+    @todo_item = @todo_list.todo_items.build
+  end
+
+  # GET todo_lists/1/todo_items/1/edit
+  def edit
+    @todo_item = @todo_list.todo_items.find(params[:id])
+  end
+
+  # POST todo_lists/1/todo_items
+  def create
+    @todo_item = @todo_list.todo_items.build(todo_item_params)
+
+    if @todo_item.save
+      redirect_to([@todo_item.todo_list, @todo_item], notice: 'Todo item was successfully created.')
+    else
+      render action: 'new'
+    end
+  end
+
+  # PUT todo_lists/1/todo_items/1
+  def update
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.update_attributes(todo_item_params)
+      flash[:success] = 'Saved todo list item.'
+      redirect_to todo_list_todo_items_path
+    else
+      flash[:error] = 'That todo item could not be saved.'
+      render action: :edit
+    end
+  end
+
+  # DELETE todo_lists/1/todo_items/1
+  def destroy
+    @todo_item.destroy
+
+    redirect_to todo_list_todo_items_url(@todo_list)
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_todo_list
+      @todo_list = TodoList.find(params[:todo_list_id])
+    end
+
+    def set_todo_item
+      @todo_item = @todo_list.todo_items.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def todo_item_params
+      params.require(:todo_item).permit(:description, :completed, :completed_at, :todo_list_id)
+    end
+end
